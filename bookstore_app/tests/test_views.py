19a443f7d85f12ced8a41114b85606bc105edaba
Shulerino <d.shulg@yandex.ru>
2022-06-21 00:57:52 +0300

    Add-test-bookinfo

--- bookstore_app/tests/test_views.py
+++ bookstore_app/tests/test_views.py
@@ -1,7 +1,12 @@
 from urllib import response
-from django.test import TestCase
+import django
+from django.conf import settings
+from django.test import TestCase, override_settings
 from bookstore_app.models import *
 from bookstore_app.forms import *
+from unittest import mock
+from django.core.files import File
+
 
 class TestIndex(TestCase):
     @classmethod
@@ -16,8 +21,8 @@ class TestIndex(TestCase):
             Book.objects.create(
                 title="title{0}".format(num+1),
                 author=Author.objects.get(id=num+1),
-                genre=Book.GENR_CORT[num+1],
-                language=Book.LANG_CORT[num+1],
+                genre=Book.GENR_CORT[num+1][0],
+                language=Book.LANG_CORT[num+1][0],
             )
         
 
@@ -80,17 +85,63 @@ class TestIndex(TestCase):
         self.assertFalse(resp2.context["books"].exists())
         self.assertEqual(resp2.context["message"], "Книги не найдены")
     
-    # def test_index_genre(self):
-    #     books=Book.objects.filter(genre=("Basnya", "Басня"))
-    #     resp1 = self.client.post(reverse('index'), {
-    #         "butsearchauthor": True,
-    #         "genre_list": 'Basnya',
-    #         })
-    #     self.assertQuerysetEqual(resp1.context["books"], books)
+    def test_index_genre(self):
+        books=Book.objects.filter(genre="Basnya")
+        resp1 = self.client.post(reverse('index'), {
+            "butsearchauthor": True,
+            "genre_list": "Basnya",
+            })
+        resp2 = self.client.post(reverse('index'), {
+            "butsearchauthor": True,
+            "genre_list": "Rasskaz",
+            })
+        self.assertQuerysetEqual(resp1.context["books"], books)
+        self.assertFalse(resp2.context["books"].exists())
+        self.assertEqual(resp2.context["message"], "Книги не найдены")
+    
+    def test_index_author_genre(self):
+        books=Book.objects.filter(author=1, genre="Basnya")
+        resp1 = self.client.post(reverse('index'), {
+            "butsearchauthor": True,
+            "author_list": 1,
+            "genre_list": "Basnya",
+            })
+        resp2 = self.client.post(reverse('index'), {
+            "butsearchauthor": True,
+            "author_list": 1,
+            "genre_list": "Rasskaz",
+            })
+        self.assertQuerysetEqual(resp1.context["books"], books)
+        self.assertFalse(resp2.context["books"].exists())
+        self.assertEqual(resp2.context["message"], "Книги не найдены")
+    
+    def test_index_language(self):
+        books=Book.objects.filter(language__in=["DEU", "ITA"])
+        resp1 = self.client.post(reverse('index'), {
+            "butsearchlanguage": True,
+            "language_list": ["DEU", "ITA"],
+            })
+        resp2 = self.client.post(reverse('index'), {
+            "butsearchauthor": True,
+            "language_list": ["ENG"],
+            })
+        self.assertQuerysetEqual(resp1.context["books"], books)
+        self.assertFalse(resp2.context["books"].exists())
+        self.assertEqual(resp2.context["message"], "Книги не найдены")
 
+
+import tempfile
+import shutil
+@override_settings(MEDIA_ROOT=tempfile.mkdtemp())
 class TestBookDetailView(TestCase):
     @classmethod
     def setUpTestData(cls):
+        # image_list=[]
+        # for num in range(5):
+        #     image=mock.MagicMock(spec=File)
+        #     image.name="image{0}.jpg".format(num+1)
+        #     image_list.append(image)
+        
         for num in range(6):
             Author.objects.create(
                 surname="surname{0}".format(num),
@@ -100,12 +151,17 @@ class TestBookDetailView(TestCase):
         for num in range(5):
             Book.objects.create(
                 title="title{0}".format(num+1),
+                #image=image_list[num],
+                summary="summary{0}".format(num+1),
                 author=Author.objects.get(id=num+1),
-                genre=Book.GENR_CORT[num+1],
-                language=Book.LANG_CORT[num+1],
+                genre=Book.GENR_CORT[num+1][0],
+                language=Book.LANG_CORT[num+1][0],
+                price=num+101,
+                count=num+1
             )
-    
+
     def test_bookdetail_view_get(self):
+        book=Book.objects.get(id=1)
         resp1 = self.client.get('/book/1')
         resp2 = self.client.get('/book/105')
         resp3 = self.client.get(reverse("book_info", kwargs={'pk': 1}))
@@ -113,6 +169,18 @@ class TestBookDetailView(TestCase):
         self.assertEqual(resp2.status_code, 404)
         self.assertEqual(resp3.status_code, 200)
         self.assertTemplateUsed(resp1, "book_info.html")
+        self.assertIn(book.title, resp1.context["book"].title)
+        #self.assertEqual(book.image, resp1.context["book"].image)
+        self.assertIn(book.summary, resp1.context["book"].summary)
+        self.assertEqual(book.author, resp1.context["book"].author)
+        self.assertIn(book.genre, resp1.context["book"].genre)
+        self.assertIn(book.language, resp1.context["book"].language)
+        self.assertEqual(book.price, resp1.context["book"].price)
+        self.assertEqual(book.count, resp1.context["book"].count)
+        print(book.image)
+        print(resp1.context["book"].image)
+
+
 
 
         
